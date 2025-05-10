ac8f868af056e3cf2c17dc0c2b8a1304df5be9cc
mary kenny <marykenny2704@yahoo.com>
2025-05-10 17:13:54 +0200

    initial commit for Boxes

--- Boxes.java
+++ Boxes.java
@@ -1,7 +1,7 @@
+import java.util.Scanner;
 public class Boxes {
     public static void main(String[] args) {
 
-        
         /* ¡Nos mudamos de piso! vamos a crear un pequeño programa que nos ayude a saber en que caja pusimos las siguientes cosas:
 
         Caja 0. Libros
@@ -14,9 +14,51 @@ public class Boxes {
         */
 
         //Crea un array con las cosas que contienen las cajas
+        String[] things = {"libros", "utensilios de cocina", "artículos de baño", "bolsos", "zapatos", "cosas de verano", "cosas de invierno"};
 
-        //Pregunta al usuario lo siguiente: Hola, soy tu asistente de la organización ¿Qué deseas buscar? (la respuesta deberá aparecer en la terminal en la misma línea)
+        //Pregunta al usuario lo siguiente: Hola, soy tu asistente de la organización ¿Qué deseas buscar?
+        // (la respuesta deberá aparecer en la terminal en la misma línea)
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Hola, soy tu asistente de la organización ¿Qué desea buscar?\t");
+        String reply = scanner.nextLine();
+        scanner.close();
 
-        //Crea un programa que te devuelva lo siguiente: <cosas> están en la caja <índice> en el caso de no existir que devuelva: <cosas> no las empacaste, oops! 
+        //Crea un programa que te devuelva lo siguiente: <cosas> están en la caja <índice> en el caso de no existir
+        //que devuelva: <cosas> no las empacaste, oops!
+        int i;
+        switch (reply) {
+            case ("libros"):
+                i = 0;
+                System.out.println(reply + " están en la caja " + i);
+                break;
+            case ("utensilios de cocina"):
+                i = 1;
+                System.out.println(reply + " están en la caja " + i);
+                break;
+            case ("artículos de baño"):
+                i = 2;
+                System.out.println(reply + " están en la caja " + i);
+                break;
+            case ("bolsos"):
+                i = 3;
+                System.out.println(reply + " están en la caja " + i);
+                break;
+            case ("zapatos"):
+                i = 4;
+                System.out.println(reply + " están en la caja " + i);
+                break;
+            case ("cosas de verano"):
+                i = 5;
+                System.out.println(reply + " están en la caja " + i);
+                break;
+            case ("cosas de invierno"):
+                i = 6;
+                System.out.println(reply + " están en la caja " + i);
+                break;
+            default:
+                System.out.println(reply + " no las empacaste, oops!");
+        }
+
+
     }
 }
--- Welcome.java
+++ Welcome.java
@@ -1,12 +1,14 @@
 public class Welcome {
     public static void main(String[] args) {
-
+// ------------- falta arreglar penúltiimo (error) y último (debe ser for each) + subir en branch develop
         //Crea un array que contenga 5 nombres de coders y con un bucle 'for-each' imprime: ¡Hola <name>, bienvenida a Java!
 
         String coders[] = {"Mary", "Mariyah", "Alexandra", "May", "Paula"};
         for (int i = 0; i < coders.length; i++) {
             System.out.println("¡Hola " + coders[i] + ", bienvenida a Java!");
         }
+
+        System.out.print("\n");
 
         //Escribe un bucle 'for' que imprima solo los siguientes números: 10 - 30 - 50 - 70 - 90 del array numbers, sin
         // modificarlo ni creando uno nuevo
@@ -15,22 +17,24 @@ public class Welcome {
             System.out.println(numbers[i]);
         }
 
-        System.out.println("\n");
+        System.out.print("\n");
 
         //Crea un array que contenga algunos números y usa un bucle 'for-each' e imprime cada número duplicado
         // (multiplicado por 2)
         int[] numArray = {1,5,9,4,80,55};
-        for (int i = 0; i < numArray.length; i++) {
-            System.out.println(numArray[i]*2);
+        for (int i: numArray) {
+            //int newNumber = (numArray[i]*2);
+            System.out.println(i*2);
         }
 
+        System.out.print("\n");
 
         //Crea un array de tipo char que contenga las vocales en minúsculas y usa un bucle 'for-each' e imprime cada
         // vocal en mayúsculas
 
         char[] charArray = {'a','c','t','h','j','z'};
-        for (int i = 0; i < charArray.length; i++) {
-            System.out.println(Character.toUpperCase(charArray[i]));
+        for (char i : charArray) {
+            System.out.println(Character.toUpperCase(i));
         }
 
     }
