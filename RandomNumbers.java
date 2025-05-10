c674865251e3e97ee8df78ebdb22cf5a1b2fda12
mary kenny <marykenny2704@yahoo.com>
2025-05-10 22:54:04 +0200

    final commit for features/RandomNumbers

--- RandomNumbers.java
+++ RandomNumbers.java
@@ -3,12 +3,8 @@ public class RandomNumbers {
         
         //Escribe un programa que genere un array de 2 dimensiones de 100 filas y 10 columnas  con números aleatorios del
         // 0 al 99, a través de un bucle anidado (Si no te suenan estos conceptos, ¡búscalos!)
-        //System.out.println(randomNumber());
-        print2DArray();
 
-
-        // ------------- arreglar randomnum puesto hasta 9 en lugar de 100 para probar
-        // --------------arreglar a 100 filas y 10 columnas, reducido para probar
+        print2DArray(100,10);
     }
 
     /**
@@ -20,10 +16,9 @@ public class RandomNumbers {
      * 1. generate random numbers from 0 to 99
      */
     public static int randomNumber() {
-        int randomNum = (int) (Math.random() * 9);
+        int randomNum = (int) (Math.random() * 100);
         return randomNum;
     }
-
 
         /**
      * Function name: print2DArray
@@ -33,25 +28,20 @@ public class RandomNumbers {
      * Inside the function:
      * 1. nested loop that prints a 2D array using the randomNumber function
      */
-        int rows = 5;
-        int columns = 2;
-        int[][] twoDArray; //= int[rows][columns];
 
-    public static int[][] print2DArray() {
-        int[][] numArray =  new int[5][2];
-        for (int i = 0; i <= 4; i++) {
-            for (int j = 0; j <= 1; j++ ) {
+    public static int[][] print2DArray(int rows, int columns) {
+        //int rows = 5;
+        //int columns = 2;
+        int[][] numArray =  new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++ ) {
                 numArray[i][j] = randomNumber();
                 System.out.print(numArray[i][j] + " ");
-            }
+                }
             System.out.println();
-        }
-
-
-            //System.out.println(numArray);
+            }
         return numArray;
         }
-
 
     }
 
