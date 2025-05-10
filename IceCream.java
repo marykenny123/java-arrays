c02d01b8cec622d8e24d20ec7eb4a186089e3ea4
mary kenny <marykenny2704@yahoo.com>
2025-05-10 19:23:49 +0200

    commit for IceCream now completed

--- IceCream.java
+++ IceCream.java
@@ -5,20 +5,36 @@ public class IceCream {
         //Vamos a hacer un programa que haga un pedido online por cada helado ordenado, vamos a escoger cuantas bolas
         // de helado le pondremos y sus sabores.
 
-        //Ejemplo de como debe mostrarse en la terminal:
         Scanner scanner = new Scanner(System.in);
         System.out.print("¿Cuántas bolas de helado quieres?: ");
         int numScoops = scanner.nextInt();
         scanner.nextLine();
         String flavours[] = new String[numScoops];
 
-        System.out.println("\n¡Perfecto! Ahora escoge de que sabor quieres cada helado:");
+        System.out.print("\n¡Perfecto! Ahora escoge de que sabor quieres cada helado:\n");
 
         for (int i = 0; i < numScoops; i++) {
             System.out.print("\t" + i + ".  ");
             flavours[i] = scanner.nextLine();
         }
+        System.out.print("\n¡Gracias por tu orden! Has pedido los siguientes sabores:\n");
 
+        for (int i = 0; i < numScoops; i++) {
+            System.out.print("\t  " + i +".  " + flavours[i] + "\n");
+        }
+
+        System.out.print("\n¿Es correcto? presiona intro para continuar\t");
+        //System.out.print(System.in.read());
+        //Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        System.out.println("\n¡Listo, tenemos tu orden! Acércate al mostrador y Robotina te entregará tu helado");
+
+        scanner.close();
+    }
+}
+
+
+        //Ejemplo de como debe mostrarse en la terminal:
         /* 
             ¿Cuántas bolas de helado quieres?: 3
 
@@ -47,6 +63,4 @@ public class IceCream {
 
         //Mira en el ejemplo los saltos de línea que has de tener.
 
-        scanner.close();
-    }
-} 
+
